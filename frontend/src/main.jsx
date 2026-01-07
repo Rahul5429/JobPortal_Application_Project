652dfd1e0b90b9c908d01f21b541f6ee54fb9690
Rahul Kumar <rahulsingh25062004@gmail.com>
2026-01-07 06:53:56 +0530

    Fix auth cookies and enable axios credentials globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Toaster } from './components/ui/sonner.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "./components/ui/sonner.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import axios from "axios";
+
+axios.defaults.withCredentials = true; //  REQUIRED
 
 const persistor = persistStore(store);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -18,5 +21,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Toaster />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
